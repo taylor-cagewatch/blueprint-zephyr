8cd47cf8912d8ccbcb830a5084102ca23c243569
Mohamed Abdelmaksoud <mtalatcise@gmail.com>
2025-05-06 13:04:46 +0200

    [Mender] Add sysbuild bootloader config & partition config

--- plugin_system/nce_fota_mender_demo/sysbuild.cmake
+++ plugin_system/nce_fota_mender_demo/sysbuild.cmake
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 #
 if(SB_CONFIG_THINGY91_STATIC_PARTITIONS_FACTORY)
-set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/thingy91_pm_static.yml CACHE INTERNAL "")
+set(PM_STATIC_YML_FILE ${CMAKE_CURRENT_LIST_DIR}/pm_static_thingy91.yml CACHE INTERNAL "")
 endif()
